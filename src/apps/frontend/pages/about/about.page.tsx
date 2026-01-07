d9491f0372cd44497b7a360713f86e64a102389f
Ritesh <riteshkd2k20@gmail.com>
2026-01-07 22:18:43 +0530

    feat: add content to About and Dashboard pages

--- src/apps/frontend/pages/about/about.page.tsx
+++ src/apps/frontend/pages/about/about.page.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 export default function About(): React.ReactElement {
   return (
     <div>
+      About Page
       <img id="companyLogo" src="/assets/img/logo.jpg" />
     </div>
   );
--- src/apps/frontend/pages/dashboard/index.tsx
+++ src/apps/frontend/pages/dashboard/index.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 
-const Dashboard: React.FC = () => <div></div>;
+const Dashboard: React.FC = () => <div>
+    Dashboard Page
+</div>;
 
 export default Dashboard;
